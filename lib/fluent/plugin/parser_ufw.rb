9f1f17e4f4b429cce3b659c16a3589c5cec11912
Takashi Masuyama <mamewotoko@gmail.com>
2017-08-03 20:54:24 +0900

    remove time field from record

--- lib/fluent/plugin/parser_ufw.rb
+++ lib/fluent/plugin/parser_ufw.rb
@@ -1,50 +1,51 @@
-require 'fluent/plugin/parser'
+require 'fluent/parser'
 
-module Fluent::Plugin
-  class UFWParser < Parser
-    # Register this parser as "time_key_value"
-    Plugin.register_parser("ufw", self)
-    @time_format = "%b %d %H:%M:%S"
-    @pattern = /(?<time>[^ ]+ [^ ]+ [^ ]+) (?<host>[^ ]+) kernel: \[[0-9.]*\] \[(?<action>[^\]]*)\] (?<body>.*)/
-#    config_param :delimiter, :string, default: " "   # delimiter is configurable with " " as default
-#    config_param :time_format, :string, default: nil # time_format is configurable
+module Fluent
+  class TextParser
+    class UFWParser < Parser
+      # Register this parser as "time_key_value"
+      Plugin.register_parser("ufw", self)
 
-    def configure(conf)
-      super
+      def configure(conf)
+        super
+        @time_format = "%b %d %H:%M:%S"
+        @pattern = /(?<time>[^ ]+ +[^ ]+ [^ ]+) (?<host>[^ ]+) kernel: \[[0-9. ]*\] \[(?<action>[^\]]*)\] (?<body>.*)/
 
-      # TimeParser class is already given. It takes a single argument as the time format
-      # to parse the time string with.
-      @time_parser = TimeParser.new(@time_format)
-    end
-
-    def parse(text)
-      m = @pattern.match(line)
-      unless m
-        yield nil, nil
-        return
+        #$log.info "ufw is configured"
+        # TimeParser class is already given. It takes a single argument as the time format
+        # to parse the time string with.
+        @time_parser = TimeParser.new(@time_format)
       end
-      time = m['time']
-      time = @time_parser.parse(time)
-      host = m['host']
-      action = m['action']
 
-      record = {
-        "time" => time,
-        "host" => host,
-        "action" => action
-      }
+      def parse(text)
+        #$log.info "parse called: $text"
+        m = @pattern.match(text)
+        unless m
+          yield nil, nil
+          return
+        end
+        time = m['time']
+        time = @time_parser.parse(time)
+        host = m['host']
+        action = m['action']
 
-      body = m['body']
-      body.split(' ').each do |pair|
-        key, value = pair.split('=',
+        record = {
+          "host" => host,
+          "action" => action
+        }
+
+        body = m['body']
+        body.split(' ').each do |pair|
+          key, value = pair.split('=', 2)
+          record[key] = value
+        end
+
+#        record.each { |key,value|
+#          $log.info "#{key} #{value}"
+#        }
+        time, record = convert_values(time, record)
+        yield time, record
       end
-      key_values.split(@delimiter).each { |kv|
-        k, v = kv.split("=", 2)
-        record[k] = v
-      }
-      time, record = convert_values(time, record)
-      yield time, record
     end
   end
 end
-
