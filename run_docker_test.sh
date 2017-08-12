5749f9623b74fe73c2385da85ccca5977ac5212b
Takashi Masuyama <mamewotoko@gmail.com>
2017-08-13 07:03:21 +0900

    modify test

--- run_docker_test.sh
+++ run_docker_test.sh
@@ -8,8 +8,6 @@ set -x
     bundle exec rake build
     docker-compose up --build -d
     sleep 20
-    docker-compose exec fluent-plugin-ufw ls /home/fluent/ufw.output
-
     docker-compose exec fluent-plugin-ufw sh -c "cat /home/fluent/ufw.output.*" | tee ufw.output.log | grep SRC
 )
 EXIT_CODE=$?
