dea04a78425ed1a953996042f8035b7d760121cf
Takashi Masuyama <mamewotoko@gmail.com>
2018-10-06 16:36:59 +0900

    output xml report as lint result

--- run_lint.sh
+++ run_lint.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 set -e
 
-rubocop
+gem install rubocop rubocop-checkstyle_formatter
+rubocop --require rubocop/formatter/checkstyle_formatter --format RuboCop::Formatter::CheckstyleFormatter --no-color $*
